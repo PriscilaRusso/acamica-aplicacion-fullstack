d7d607f8ec79a148ef3bd648a17401890fe3a1db
Priscila Russo <priscilarusso2017@gmail.com>
2022-02-16 11:00:47 -0300

    Cree el componente Feed y le di estilos.

---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./Profile.module.css";
+import Tweets from '../Tweets/Tweets';
+
+export default function Profile () {
+    return (
+        <div>
+            <header className={styles.profile}>
+                <img className={styles.back} src="./img/back.svg" alt="Back." />
+                <img className={styles.nameImg} src="./img/username2.svg" alt="Nombre." />
+            </header>
+            <div className={styles.info}>
+                <img className={styles.picProfile1} src="./img/profilephoto.svg" alt="Foto de perfil." />
+                <div className={styles.user}> USERNAME</div>
+            </div>
+           <Tweets/>
+        </div>
+    )
+}
