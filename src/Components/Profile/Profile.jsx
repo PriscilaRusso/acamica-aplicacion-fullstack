3c14c2c2cad1bb5d9a6adb129fb17405be69be46
Priscila Russo <priscilarusso2017@gmail.com>
2022-02-23 12:54:49 -0300

    Hice el Perfil del otro Usuario + css y su función.

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -1,19 +1,68 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styles from "./Profile.module.css";
 import Tweets from '../Tweets/Tweets';
+import { Link, Navigate } from "react-router-dom";
+import { firestore } from "../../firebase";
+import { collections } from "../../firebase/firebaseConfig";
+import { useParams } from "react-router-dom";
 
-export default function Profile () {
+export default function Profile ({user}) {
+
+    const { uid } = useParams();
+    const [userProfile, setUserProfile] = useState();
+    const [redirect, setRedirect] = useState(false);
+    const [posts, setPosts] = useState([]);
+
+
+    useEffect(() => {
+        firestore
+            .collection(collections.USERS)
+            .doc(uid)
+            .get()
+            .then((doc) => {
+                if (doc.exists) {
+                    setUserProfile(doc.data());
+                } else {
+                    setRedirect(true);
+            
+            }})
+            .catch((error) => setRedirect(true));
+        const unsuscribe = firestore
+            .collection(collections.TWEETS)
+            .where("uid", "==", uid)
+            .onSnapshot((snapshot) => {
+                const posts = snapshot.docs.map((doc) => {
+                    return {
+                        id: doc.id,
+                        ...doc.data(),
+                    };
+                });
+                setPosts(posts);
+              
+            });
+        return () => unsuscribe();
+    }, []);
+
+   
+    if (!user) {
+        return <Navigate replace to="/login" />;
+    }
+
     return (
         <div>
+            <Link    to="/"> 
             <header className={styles.profile}>
-                <img className={styles.back} src="./img/back.svg" alt="Back." />
-                <img className={styles.nameImg} src="./img/username2.svg" alt="Nombre." />
+            <img src="/img/back.svg" alt="Back." /> 
+            <div className={styles.nameImg}> {posts[0]?.autor} </div>
             </header>
+            </Link>
             <div className={styles.info}>
-                <img className={styles.picProfile1} src="./img/profilephoto.svg" alt="Foto de perfil." />
-                <div className={styles.user}> USERNAME</div>
+                <img className={styles.picProfile1} src={posts[0]?.pic} alt="Foto de perfil." />
+                <div className={styles.user}> {posts[0]?.autor} </div>
             </div>
-           <Tweets/>
+            {posts.map((tweet, idx) => {
+                    return <Tweets key={idx} user={user} tweet={tweet} />;
+                })}
         </div>
     )
 }
