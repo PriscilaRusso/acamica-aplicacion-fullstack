e8a7faacc0005a3e56807753457af89b1a46ed94
Priscila Russo <priscilarusso2017@gmail.com>
2022-02-23 12:53:02 -0300

    Arregle css y le di funcion al tweet, like, borrar

--- src/Components/Tweets/Tweets.jsx
+++ src/Components/Tweets/Tweets.jsx
@@ -1,22 +1,59 @@
-import React from 'react';
-import styles from './Tweets.module.css'
+import React, {useState} from 'react';
+import styles from './Tweets.module.css';
+import { firestore } from "../../firebase";
+import { collections } from "../../firebase/firebaseConfig";
+import Alert from "../Alert/Alert"
+import { Link } from 'react-router-dom';
 
-export default function Tweets () {
+
+export default function Tweets ({tweet, user}) {
+    
+    const isLiked= tweet.likes.includes(user?.uid);
+    const isUser = tweet.uid === user?.uid;
+    const [open, setOpen] = useState(false);
+    
+
+    const handleLike = () => {
+        if (isLiked) {
+            firestore
+                .doc(`${collections.TWEETS}/${tweet.id}`)
+                .update({ likes: tweet.likes.filter((uid) => uid !== user?.uid) });
+        } else {
+            firestore
+                .doc(`${collections.TWEETS}/${tweet.id}`)
+                .update({ likes: [...tweet.likes, user?.uid] });
+        }
+    };
+
+    const handleDelete = () => {
+       firestore.doc(`${collections.TWEETS}/${tweet.id}`).delete()
+    }
+    
+    const onCloseModal = () => setOpen(false)
+    
     return (
+
         <div className={styles.containerTweets}>
-                    <img className={styles.profilePhoto3} src="./img/profilePhoto.svg" alt="Foto del usuario." />
+            <Alert open={open} onClose={onCloseModal} onConfirm={handleDelete}></Alert>
+                   <Link to={`/users/${isUser ? "me" : tweet.uid}`}>
+                    <img className={styles.profilePhoto3} src={tweet.pic} alt="Foto del usuario." />
+                   </Link>
                 <div className={styles.boxTweets}>
                     <div className={styles.infoTw}>
-                        <div className={styles.username}>USERNAME</div>
-                        <img className={styles.trash} src="./img/trash.svg" alt="Eliminar." />
+                        <div className={styles.username}>{tweet.autor}</div>
+                        {isUser && (<img className={styles.trash} onClick={()=> setOpen (true)} src="/img/trash.svg" alt="Eliminar." />)}
                     </div>  
-                    <div className={styles.parrafo}>HolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHolaHola</div>
-                    <div className={styles.like}>
-                        <img src="./img/like.svg" alt="Like." />
-                        <span>100</span>
+                    <div className={styles.parrafo}>{tweet.tweet}</div>
+                    <div onClick={handleLike} className={styles.like}>
+                        {isLiked ? (
+                        <img src="/img/like.svg" alt="Corazón Rojo." />
+                    ) : (
+                        <img className={styles.heart} src="/img/CorazonVacio.svg" alt="Corazón Vacio." />
+                    )}
+                        <span>{tweet.likes.length}</span>
                     </div>
                 </div>
         </div>
     )
-}
+};
 
